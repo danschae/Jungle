e3eff839a1993dc3e4d2c7a266db74a97f8c65b8
Daniel <danschae@gmail.com>
2020-11-10 18:23:22 -0500

    implement dashboard statistics

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name =>  "#{ENV["username"]}", :password => "#{ENV["password"]}"
   def show
+    @counted_products = Product.count
+    @counted_categories = Category.count
   end
 end
