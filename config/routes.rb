b516a455886cccd4462f1847989b30c63ca0f0bb
Daniel <danschae@gmail.com>
2020-11-10 19:46:44 -0500

    implement about us page and route

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,15 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
+  
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+  
+  get '/about' => 'about#show'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
