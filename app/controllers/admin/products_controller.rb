58c03db3237169908508a7688fe89bcb62aae223
Daniel <danschae@gmail.com>
2020-11-11 10:36:29 -0500

    implement admin category feature

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  puts "this is my ENV:#{ENV['username']}"
   http_basic_authenticate_with :name =>  "#{ENV["username"]}", :password => "#{ENV["password"]}"
   def index
     @products = Product.order(id: :desc).all
