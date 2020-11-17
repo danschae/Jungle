4deab2ae8e537c4f2db71063a50acf1c60251da9
Daniel <danschae@gmail.com>
2020-11-17 12:15:01 -0500

    add addcart spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor can see product page", type: :feature, js: true do
-
+RSpec.feature "AddToCarts", type: :feature do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,15 +16,13 @@ RSpec.feature "Visitor can see product page", type: :feature, js: true do
     end
   end
 
-  scenario "They click on product img and is led to the project page" do
+  scenario "They click on add to cart" do
     # ACT
     visit root_path
-    first('h4').click
-    
-    # DEBUG
-    save_screenshot
-
+    page.all('.actions').first.click_on 'Add'
+   
+  
     # VERIFY
-    expect(page).to have_text '64.99'
+    expect(page).to have_content 'My Cart (1)'
   end
 end
